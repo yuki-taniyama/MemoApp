fc3c2b036dc72e2dd3cdfd081e9756848023bf3e
yuki <92778035+yuki-taniyama@users.noreply.github.com>
2021-11-20 11:51:06 +0900

    Create custom icon

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -2,6 +2,8 @@ import { Feather } from "@expo/vector-icons";
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+import Icon from './Icon';
+
 export default function MemoList() {
     return (
         <View>
@@ -11,7 +13,7 @@ export default function MemoList() {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-            <Feather name="x" size={16} color="#B0B0B0" />
+            <Icon name="delete" size={24} color="#B0B0B0" />
           </View>
         </View>
 
@@ -21,7 +23,7 @@ export default function MemoList() {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+            <Icon name="delete" size={24} color="#B0B0B0" />
           </View>
         </View>
 
@@ -31,7 +33,7 @@ export default function MemoList() {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+            <Icon name="delete" size={24} color="#B0B0B0" />
           </View>
         </View>
       </View>
