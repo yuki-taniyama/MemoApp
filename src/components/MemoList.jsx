8312ef1b37677c67f640c217e02548ddaf8038fe
yuki <92778035+yuki-taniyama@users.noreply.github.com>
2021-11-23 22:20:28 +0900

    Fetch data from firestore on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -4,6 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import { shape, string, instanceOf, arrayOf } from 'prop-types';
 
 import Icon from './Icon';
+import { dateToString } from "../utils";
 
 export default function MemoList(props) {
   const { memos } = props;
@@ -13,11 +14,11 @@ export default function MemoList(props) {
     return (
       <TouchableOpacity
       style={styles.memoListItem}
-      onPress={() => { navigation.navigate('MemoDetail'); }}
+      onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
       <View>
         <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-        <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+        <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
       </View>
       <TouchableOpacity
         style={styles.memoDelete}
