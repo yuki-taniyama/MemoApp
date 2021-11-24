934c429c5371226b015a818c41babe7bf80ed07e
yuki <92778035+yuki-taniyama@users.noreply.github.com>
2021-11-24 09:23:24 +0900

    Create Loading component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -3,11 +3,13 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'reac
 import firebase from "firebase";
 
 import Button from "../components/Button";
+import Loading from "../components/Loading";
 
 export default function LogInScreen(props) {
     const { navigation } = props;
     const [email, setEmail] =useState('');
-    const [password, setPassword] =useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -16,12 +18,15 @@ export default function LogInScreen(props) {
                     index: 0,
                     routes: [{ name: 'MemoList' }],
                 });
+            } else {
+                setLoading(false);
             }
         });
         return unsubscribe;
     }, []);
 
     function handlePress() {
+        setLoading(true);
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredential) =>　{
                 const { user } = userCredential;
@@ -33,11 +38,15 @@ export default function LogInScreen(props) {
             })
             .catch((error) => {
                 Alert.alert(error.code);
+            })
+            .then(() => {
+                setLoading(false);
             });
     }
 
     return (
         <View style={styles.container} >
+            <Loading isLoading={isLoading} />
         <View style={styles.inner}>
             <Text style={styles.title}>Log In</Text>
             <TextInput
